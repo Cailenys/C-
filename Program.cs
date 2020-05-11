ebae1f9103f99a77b751f2f96e73955f94ac9ae8
Cailenys <cailenys.salazar@gmail.com>
2020-05-11 08:20:04 -0600

    First commit - The parser options are set

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Parser_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+             Option = "1";
+
+            switch(Option){
+                case "1": 
+                    Console.WriteLine("Please enter filename");
+                    string fileName = Console.ReadLine();
+                    Console.WriteLine("The filename will be stored in a local variable");
+                   
+                case "2":
+                    Console.WriteLine("Please enter filename");
+                    Console.WriteLine("filName: " +"" + fileName);
+                    
+              
+                case "3":
+
+                int Option = 0; 
+
+                while (option < 3){ 
+                    option++;
+                    Console.WriteLine(option);
+                }
+                break;
+               
+            }       
+
+        }
+    }
+}
